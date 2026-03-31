61261bad311258ddfd0d90653f6c88c066559f4f
gabriel <red2003ho@outlook.com>
2026-03-31 19:54:10 -0300

    mudanca

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Eu voce e o zoboomafoo");
+Console.WriteLine("Vamos Juntos Descobrir");
+
+Console.WriteLine("Um novo mundo vamos curtir");
+Console.WriteLine("Os Animais Conhecer");
+
+Console.WriteLIne("Voce vai se surpreender");
+Console.WriteLine("Elefantes e macacos chegam na floresta");
+
+Console.WriteLIne("E ate os cachorrinhos");
+Console.WriteLIne("Vem para a festa");
+Console.WriteLIne("Eu voce e o zoboomafoo");
